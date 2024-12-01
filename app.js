5953d41bd920924fdc1431ead29e52bc13caedba
Saurabh Mishra <student001599@gmail.com>
2024-12-01 21:34:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -48,8 +48,10 @@ async function popularmovies(num) {
       let button = document.createElement("button");
       button.className = "btn";
       movieBox.appendChild(button);
-      let a = document.createElement("a");
+      let a = document.createElement("div");
       a.className = "see-in-detail";
+      a.innerHTML = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                   see in datail </button>`;
       let url3 = `https://api.themoviedb.org/3/movie/${m.id}?api_key=dd108ed11f35010d3de9661e82890d62`;
       let moviepage = 1;
       a.addEventListener("click", async (moviepage) => {
@@ -60,10 +62,10 @@ async function popularmovies(num) {
           },
         };
         let res = await axios.get(url3, config);
-        console.log(res.data);
+        seeInDetail(res);
       });
-      a.innerText = "See In Detail";
-      button.appendChild(a);
+      // a.innerText = "See In Detail";
+      movieBox.appendChild(a);
     }
     let btn3 = document.createElement("button");
     btn3.className = "btn btn-dark";
@@ -123,8 +125,10 @@ async function trendingmovies(num3) {
       let button = document.createElement("button");
       button.className = "btn";
       movieBox.appendChild(button);
-      let a = document.createElement("a");
+      let a = document.createElement("div");
       a.className = "see-in-detail";
+      a.innerHTML = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                   see in datail </button>`;
       let url3 = `https://api.themoviedb.org/3/movie/${m.id}?api_key=dd108ed11f35010d3de9661e82890d62`;
       let moviepage = 1;
       a.addEventListener("click", async (moviepage) => {
@@ -135,10 +139,10 @@ async function trendingmovies(num3) {
           },
         };
         let res = await axios.get(url3, config);
-        console.log(res.data);
+        seeInDetail(res);
       });
-      a.innerText = "See In Detail";
-      button.appendChild(a);
+      // a.innerText = "See In Detail";
+      movieBox.appendChild(a);
     }
     let btn3 = document.createElement("button");
     btn3.className = "btn btn-dark";
@@ -164,7 +168,6 @@ btn.addEventListener("click", async () => {
   ct3.style.display = "none";
   ct4.style.display = "block";
   let search = inp.value;
-  console.log("btn");
   await movies(search);
 });
 
@@ -172,8 +175,9 @@ let url =
   "https://api.themoviedb.org/3/search/movie?api_key=dd108ed11f35010d3de9661e82890d62&query=";
 let num2 = 1;
 async function movies(search) {
+  ct4.innerText = "";
   let h1 = document.createElement("h1");
-  h1.innerText = `Related movies : ${search}`;
+  h1.innerText = `Related movies : ${search} Page: ${num2}`;
   ct4.appendChild(h1);
   try {
     let config = {
@@ -182,7 +186,10 @@ async function movies(search) {
         Key: "dd108ed11f35010d3de9661e82890d62",
       },
     };
-    let res = await axios.get(url + search, config);
+    let res = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=dd108ed11f35010d3de9661e82890d62&query=${search}&page=${num2}`,
+      config
+    );
     console.log(res.data);
     let movies = res.data.results;
     for (m of movies) {
@@ -205,26 +212,164 @@ async function movies(search) {
       let button = document.createElement("button");
       button.className = "btn";
       movieBox.appendChild(button);
-      let a = document.createElement("a");
-      a.setAttribute(
-        "href",
-        `https://api.themoviedb.org/3/movie/${m.id}?api_key=dd108ed11f35010d3de9661e82890d62`
-      );
-      a.innerText = "See In Detail";
-      button.appendChild(a);
+      let a = document.createElement("div");
+      a.className = "see-in-detail";
+      a.innerHTML = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                   see in datail </button>`;
+      let url3 = `https://api.themoviedb.org/3/movie/${m.id}?api_key=dd108ed11f35010d3de9661e82890d62`;
+      let moviepage = 1;
+      a.addEventListener("click", async (moviepage) => {
+        let config = {
+          Headers: {
+            Accept: "application/jason",
+            Key: "dd108ed11f35010d3de9661e82890d62",
+          },
+        };
+        let res = await axios.get(url3, config);
+        seeInDetail(res);
+      });
+      // a.innerText = "See In Detail";
+      movieBox.appendChild(a);
     }
     let btn3 = document.createElement("button");
     btn3.className = "btn btn-dark";
     btn3.innerText = "NEXT";
-    num2 = num2 + 1;
-    btn3.onclick = (search, num2) => {
-      url = `https://api.themoviedb.org/3/search/movie?api_key=dd108ed11f35010d3de9661e82890d62&query=${search}&page=`;
-      search = num2;
-      movies(search);
+    btn3.onclick = () => {
+      console.log(search);
+      ct4.innerHTML = "";
+      num2 = num2 + 1;
+      moviesnext(search);
     };
     ct4.appendChild(btn3);
   } catch (e) {
     console.log("Error: ", e);
     return "Error !";
   }
+}
+
+///next
+let num5 = 2;
+async function moviesnext(search) {
+  ct4.innerText = "";
+  let h1 = document.createElement("h1");
+  h1.innerText = `Related movies : ${search} Page: ${num5}`;
+  ct4.appendChild(h1);
+  try {
+    let config = {
+      Headers: {
+        Accept: "application/jason",
+        Key: "dd108ed11f35010d3de9661e82890d62",
+      },
+    };
+    let res = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=dd108ed11f35010d3de9661e82890d62&query=${search}&page=${num5}`,
+      config
+    );
+    console.log(res.data);
+    let movies = res.data.results;
+    for (m of movies) {
+      let movieBox = document.createElement("div");
+      movieBox.className = "movie-contenar";
+      ct4.appendChild(movieBox);
+      let img = document.createElement("img");
+      img.className = "movie-img";
+      img.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500${m.poster_path}`
+      );
+      movieBox.appendChild(img);
+      let h4 = document.createElement("h4");
+      h4.innerText = m.original_title;
+      movieBox.appendChild(h4);
+      let p = document.createElement("p");
+      p.innerHTML = `Language: ${m.original_language}, Ratting: ${m.vote_average} <br> Release Date : ${m.release_date}`;
+      movieBox.appendChild(p);
+      let button = document.createElement("button");
+      button.className = "btn";
+      movieBox.appendChild(button);
+      let a = document.createElement("div");
+      a.className = "see-in-detail";
+      a.innerHTML = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                   see in datail </button>`;
+      let url3 = `https://api.themoviedb.org/3/movie/${m.id}?api_key=dd108ed11f35010d3de9661e82890d62`;
+      let moviepage = 1;
+      a.addEventListener("click", async (moviepage) => {
+        let config = {
+          Headers: {
+            Accept: "application/jason",
+            Key: "dd108ed11f35010d3de9661e82890d62",
+          },
+        };
+        let res = await axios.get(url3, config);
+        seeInDetail(res);
+      });
+      // a.innerText = "See In Detail";
+      movieBox.appendChild(a);
+    }
+    let btn3 = document.createElement("button");
+    btn3.className = "btn btn-dark";
+    btn3.innerText = "NEXT";
+    btn3.onclick = () => {
+      num5 = num5 + 1;
+      moviesnext(search);
+    };
+    ct4.appendChild(btn3);
+  } catch (e) {
+    console.log("Error: ", e);
+    return "Error !";
+  }
+}
+
+///see in detailed
+function seeInDetail(res) {
+  let mh1 = document.querySelector("#staticBackdropLabel");
+  mh1.innerText = res.data.original_title;
+
+  let md = document.querySelector(".modal-body");
+  md.innerHTML = "";
+  let p1 = document.createElement("p");
+  p1.innerText = res.data.overview;
+  md.appendChild(p1);
+  if (res.data.adult == true) {
+    let adult = document.createElement("div");
+    adult.className = "btn btn-danger";
+    adult.innerText = "adult";
+    md.appendChild(adult);
+  }
+  let bkdpath = document.createElement("img");
+  bkdpath.className = "movie-img";
+  bkdpath.setAttribute(
+    "src",
+    `https://image.tmdb.org/t/p/w500${res.data.backdrop_path}`
+  );
+  md.appendChild(bkdpath);
+  let dd = document.createElement("div");
+  md.appendChild(dd);
+  let ddname = document.createElement("a");
+  let ddimg1 = document.createElement("img");
+  let ddimg2 = document.createElement("img");
+  ddname.innerText = `Belongs to ${res.data.belongs_to_collection.name}`;
+  ddimg1.className = "smimg";
+  ddimg1.setAttribute(
+    "src",
+    `https://image.tmdb.org/t/p/w500${res.data.belongs_to_collection.backdrop_path}`
+  );
+  ddimg2.className = "smimg";
+  ddimg2.setAttribute(
+    "src",
+    `https://image.tmdb.org/t/p/w500${res.data.belongs_to_collection.poster_path}`
+  );
+  dd.appendChild(ddname);
+  dd.appendChild(ddimg1);
+  dd.appendChild(ddimg2);
+  let p2 = document.createElement("p");
+  p2.innerHTML = `Budget: ${res.data.budget},<br> Origin Country: ${res.data.origin_country},
+        <br> Original Language: ${res.data.original_language},
+         <br> Release Date: ${res.data.release_date}, <br>
+         Revenue : ${res.data.revanue},<br> Tagline: ${res.data.tagline}`;
+  md.appendChild(p2);
+  let link = document.createElement("a");
+  link.setAttribute("href", res.data.homepage);
+  link.innerText = "Link ->";
+  md.appendChild(link);
 }
